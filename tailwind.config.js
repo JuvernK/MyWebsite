529e223f5fdd81e54d1c709d8cfe0c790778b712
Juvern <juvern0621@hotmail.com>
2024-04-17 12:39:49 +0800

    add basic login page UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'grey': '#e9ecef',
+      'success': '#009485',
+      'warning': '#ff9900',
+      'error': '#ff5724',
+    },
   },
   plugins: [],
 }
