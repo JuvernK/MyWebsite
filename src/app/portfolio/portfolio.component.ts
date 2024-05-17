f7f64cd9666f7b88197e7728e29f1fbf63882e2f
Juvern <juvern0621@hotmail.com>
2024-05-18 00:01:57 +0800

    add color to tags

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProjectCardComponent } from './project-card/project-card.component';
+import { Project } from '../models/project';
+import { Tag } from '../models/tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -11,6 +13,16 @@ import { ProjectCardComponent } from './project-card/project-card.component';
   imports: [RouterLink, RouterLinkActive, ProjectCardComponent],
 })
 export class PortfolioComponent {
+  project: Project = {
+    id: 0,
+    name: 'Sample Angular App',
+    summary: 'Test Description',
+    description: '',
+    projectUrl: '',
+    images: [],
+    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+  };
+
   constructor(private titleService: Title) {
     this.titleService.setTitle("Juvern's web - Portfolio");
   }
