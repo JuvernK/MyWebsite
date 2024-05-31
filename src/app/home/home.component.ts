dc9e366417c67ec4f9def352ca48e02967886ca9
Juvern <juvern0621@hotmail.com>
2024-05-31 23:11:19 +0800

    Implement pop up dialog and carousel for porfolio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ProjectsService } from '../service/projects.service';
+import { Project } from '../models/project';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +11,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  constructor(private titleService: Title) {
+export class HomeComponent implements OnInit {
+  featuredProjects = {} as Project;
+
+  constructor(
+    private titleService: Title,
+    private projectsService: ProjectsService
+  ) {
     this.titleService.setTitle("Juvern's web - Home Page");
+  }
+  ngOnInit(): void {
+    this.featuredProjects = this.projectsService.getProjectByID(0);
   }
 }
