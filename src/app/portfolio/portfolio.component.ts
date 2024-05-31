e102b4c44dab15e3ae65968236ec5b6a97819a06
Juvern <juvern0621@hotmail.com>
2024-05-31 23:11:19 +0800

    Implement pop up dialog and carousel for porfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,29 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { Project } from '../models/project';
 import { Tag } from '../models/tag';
+import { CommonModule } from '@angular/common';
+import { ProjectsService } from '../service/projects.service';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css',
-  imports: [RouterLink, RouterLinkActive, ProjectCardComponent],
+  imports: [RouterLink, RouterLinkActive, ProjectCardComponent, CommonModule],
 })
-export class PortfolioComponent {
-  project: Project = {
-    id: 0,
-    name: 'Sample Angular App',
-    summary: 'Test Description',
-    description: '',
-    projectUrl: '',
-    images: [],
-    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-  };
+export class PortfolioComponent implements OnInit {
+  projects = {} as Project[];
 
-  constructor(private titleService: Title) {
+  constructor(
+    private titleService: Title,
+    private projectsService: ProjectsService
+  ) {
     this.titleService.setTitle("Juvern's web - Portfolio");
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.projects = this.projectsService.getProjects();
   }
 }
