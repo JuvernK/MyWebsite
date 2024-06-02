60d13993cd0acfb7efcae26e987e694f9e4ca2bc
Juvern <juvern0621@hotmail.com>
2024-06-03 00:02:28 +0800

    Add carousel to pop up modal

--- src/app/portfolio/project-modal/project-modal.component.ts
+++ src/app/portfolio/project-modal/project-modal.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Project } from '../../models/project';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule, MdbCarouselModule],
   templateUrl: './project-modal.component.html',
-  styleUrl: './project-modal.component.css'
+  styleUrl: './project-modal.component.css',
 })
-export class ProjectModalComponent {
+export class ProjectModalComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ProjectModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { project: Project }
+  ) {}
 
+  ngOnInit(): void {
+    console.log(this.data.project);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
 }
