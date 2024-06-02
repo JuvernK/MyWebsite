19f029403120f8a3a2760579a4b55c3f3d04e131
Juvern <juvern0621@hotmail.com>
2024-06-02 22:37:52 +0800

    Implement pop up for portfolio

--- src/app/portfolio/project-card/project-card.component.ts
+++ src/app/portfolio/project-card/project-card.component.ts
@@ -3,14 +3,36 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Project } from '../../models/project';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ProjectModalComponent } from '../project-modal/project-modal.component';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    ProjectModalComponent,
+  ],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css',
 })
 export class ProjectCardComponent {
-  @Input() project?: Project;
+  @Input() project = {} as Project;
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ProjectModalComponent, {
+      width: '40rem',
+      height: '45rem',
+      data: { project: this.project },
+    });
+
+    // TODO: Remove cosole.log if no further implementation is needed
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
