744763904ba6f91ad4d7ba0e0d38d10253a17386
Juvern <juvern0621@hotmail.com>
2024-09-18 18:11:28 +1000

    add resume section

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AccordionModule } from 'primeng/accordion';
+import { ResumeContentService } from '../service/resume-content.service';
+import { ResumeContent } from '../models/project';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, AccordionModule, CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css',
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+
+  resumeContent: ResumeContent[] = []
+  expandedIndex: number = 0;
+
+  constructor(private titleService: Title,
+              private resumeContentService: ResumeContentService, 
+              private renderer: Renderer2) {
     this.titleService.setTitle("Juvern's web - Resume");
   }
+
+  ngOnInit(): void {
+    this.resumeContent = this.resumeContentService.getResumeContent();
+  }
+
+  DownloadResume() {
+    let link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/Resume.pdf');
+    link.setAttribute('download', 'JuvernResume.pdf');
+    link.click();
+    link.remove();
+  }
+
 }
+ 
